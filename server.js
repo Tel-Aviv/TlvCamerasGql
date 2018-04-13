577838d34122e16f8e08640e55d6332fdee619eb
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-13 03:18:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { graphiqlExpress } from 'graphql-server';
+import graphqlHTTP from 'express-graphql';
+import { createServer } from 'http';
 import path from 'path';
 import cors from 'cors';
+import { execute, subscribe } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { expressPlayground } from 'graphql-playground-middleware';
 
 import { schema } from './schemas/schema';
 
@@ -12,9 +18,22 @@ graphQLServer.use('*', cors({
                     origin: '*'
                   })
        );
+graphQLServer.use('/graphql',
+       bodyParser.json(),
+       graphqlHTTP({
+                     schema: schema,
+                     graphiql: process.env.NODE_ENV === 'development'
+                 })
+);
 
 const PORT = process.env.port || 3002;
 
+graphQLServer.use('/playground',
+                  expressPlayground({
+                                      endpoint: '/graphql',
+                                      subscriptionEndpoint: `ws://localhost:${PORT}/subscriptions`
+                                    })
+                );
 graphQLServer.use('/graphiql',
       graphiqlExpress({
           endpointURL: '/graphql',
@@ -26,5 +45,15 @@ const websocketServer = createServer(graphQLServer);
 
 websocketServer.listen(PORT, () => {
     console.log(`Websocket Server is listening on: ${PORT}`);
+
+    // Set up the WebSocket for handling GraphQL subscriptions
+    new SubscriptionServer({
+      execute,
+      subscribe,
+      schema
+    }, {
+      server: websocketServer,
+      path: '/subscriptions',
+    });
   }
 );
