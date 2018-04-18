18d5f73747c0581231ca1523e80cdce58bccce6b
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-19 01:10:23 +0300

    Update cameraAliases.js

--- schemas/cameraAliases.js
+++ schemas/cameraAliases.js
@@ -1,5 +1,20 @@
 const cameraAliases = {
-    '29866' : '71'
+    '4268' : {
+               alias: 71,
+               url: 'https://10.81.1.10/webrtc/index.html?10.81.1.10:8000&from=admin&to=20&mode=recv'
+             },
+    '2309': {
+              alias: 23,
+              url: 'https://10.81.1.10/webrtc/index.html?10.81.1.10:8000&from=admin&to=8&mode=recv'
+            },
+    '29944': {
+              alias: 24,
+              url: 'https://10.81.1.10/webrtc/index.html?10.81.1.10:8000&from=admin&to=8&mode=recv'
+    },
+    '29866': {
+              alias: 51,
+              url: 'https://10.81.1.10/webrtc/index.html?10.81.1.10:8000&from=admin&to=6&mode=recv'
+            }
 };
 
 module.exports = cameraAliases;
